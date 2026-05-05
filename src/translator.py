9c74ef0a0bdcac98925cdb15647d10a93af70022
maksymiliangedlek <maksymilian.gedlek@gmail.com>
2026-05-05 20:45:06 +0200

    Added div and section

--- src/translator.py
+++ src/translator.py
@@ -14,12 +14,18 @@ class HtmlTranslator(Transformer):
         return str(h)
 
     def put_heading(self, args):
+        if len(args) == 2:
+            return f"<h1 class={args[0]}>{args[1]}</h1>"
         return f"<h1>{args[0]}</h1>"
 
     def put_subtitle(self, args):
+        if len(args) == 2:
+            return f"<h1 class={args[0]}>{args[1]}</h1>"
         return f"<h2>{args[0]}</h2>"
 
     def put_paragraph(self, args):
+        if len(args) == 2:
+            return f"<h1 class={args[0]}>{args[1]}</h1>"
         return f"<p>{args[0]}</p>"
 
     def put_image(self, args):
@@ -49,6 +55,16 @@ class HtmlTranslator(Transformer):
         url, label = args
         return f'<a href="{url}">{label}</a>'
 
+    def put_div(self,args):
+        content_class = args[0]
+        content = "".join(args[1:])
+        return f"<div class={content_class}>\n{content}</div>"
+
+    def put_section(self,args):
+        content_class = args[0]
+        content = "".join(args[1:])
+        return f"<section class={content_class}>\n{content}</section>"
+
     def bg_color(self, _): return "background-color"
 
     def text_color(self, _): return "color"
@@ -65,4 +81,34 @@ class HtmlTranslator(Transformer):
         return f"<style>\n.{style_name} {{\n  {content}\n}}\n</style>"
 
     def start(self, children):
-        return "\n".join(children)
+        head_elements = []
+        body_elements = []
+
+        for element in children:
+            if element.startswith("<style>"):
+                head_elements.append(element)
+            else:
+                body_elements.append(element)
+
+        head_content = "\n  ".join(head_elements)
+        body_content = "\n  ".join(body_elements)
+
+        html_template = f"""<!DOCTYPE html>
+<html lang="pl">
+<head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+{head_content}
+
+</head>
+
+<body>
+
+{body_content}
+
+</body>
+
+</html>"""
+
+        return html_template
