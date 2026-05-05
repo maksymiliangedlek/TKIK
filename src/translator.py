742eca9bcd863129d475d9d7ec10a65c624ad583
maksymiliangedlek <maksymilian.gedlek@gmail.com>
2026-05-05 18:12:50 +0200

    Added lists ordered and unordered

--- src/translator.py
+++ src/translator.py
@@ -26,6 +26,25 @@ class HtmlTranslator(Transformer):
         path = args[0]
         return f'<img src="{path}" alt="image">'
 
+    def item(self, args):
+        content = args[0]
+        return f"  <li>{content}</li>"
+
+    def put_list(self, args):
+        if args and str(args[0]).upper() in ["ORDERED", "UNORDERED"]:
+            list_type = str(args[0]).upper()
+            items = args[1:]
+        else:
+            list_type = "UNORDERED"
+            items = args
+
+        content = "\n".join(items)
+
+        if list_type == "ORDERED":
+            return f"<ol>\n{content}\n</ol>"
+        else:
+            return f"<ul>\n{content}\n</ul>"
+
     def put_hyperlink(self, args):
         url, label = args
         return f'<a href="{url}">{label}</a>'
